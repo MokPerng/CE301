8212019abbaee8eab2ac03a4273b3533a21f2e9c
MokPerng <109357262+MokPerng@users.noreply.github.com>
2023-05-06 00:19:26 +0800

    version 0.05

---
+++ Fass World/battleScene.js
@@ -0,0 +1,135 @@
+const battleBackgroundImage = new Image()
+battleBackgroundImage.src = './img/battleBackground.png'
+const battleBackground = new Sprite({
+  position: {
+    x: 0,
+    y: 0
+  },
+  image: battleBackgroundImage
+})
+
+let enemy
+let partner
+let renderedSprites
+let battleAnimationId
+let queue
+
+function BattleScene() {
+  document.querySelector('#userInterface').style.display = 'block'
+  document.querySelector('#dialogueBox').style.display = 'none'
+  document.querySelector('#enemyHealthBar').style.width = '100%'
+  document.querySelector('#playerHealthBar').style.width = '100%'
+  document.querySelector('#attacksBox').replaceChildren()
+
+  enemy = new Monster(monsters.Draggle)
+  partner = new Monster(monsters.Emby)
+  renderedSprites = [enemy, partner]
+  queue = []
+
+  partner.attacks.forEach((attack) => {
+    const button = document.createElement('button')
+    button.innerHTML = attack.name
+    document.querySelector('#attacksBox').append(button)
+  })
+
+  // our event listeners for our buttons (attack)
+  document.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const selectedAttack = attacks[e.currentTarget.innerHTML]
+      partner.attack({
+        attack: selectedAttack,
+        recipient: enemy,
+        renderedSprites
+      })
+
+      if (enemy.health <= 0) {
+        queue.push(() => {
+          enemy.faint()
+        })
+        queue.push(() => {
+          // fade back to black
+          gsap.to('#overlappingDiv', {
+            opacity: 1,
+            onComplete: () => {
+              cancelAnimationFrame(battleAnimationId)
+              animate()
+              document.querySelector('#userInterface').style.display = 'none'
+
+              gsap.to('#overlappingDiv', {
+                opacity: 0
+              })
+
+              battle.initiated = false
+              audio.Map.play()
+            }
+          })
+        })
+      }
+
+      // draggle or enemy attacks right here
+      const randomAttack =
+        enemy.attacks[Math.floor(Math.random() * enemy.attacks.length)]
+
+      queue.push(() => {
+        enemy.attack({
+          attack: randomAttack,
+          recipient: partner,
+          renderedSprites
+        })
+
+        if (partner.health <= 0) {
+          queue.push(() => {
+            partner.faint()
+          })
+
+          queue.push(() => {
+            // fade back to black
+            gsap.to('#overlappingDiv', {
+              opacity: 1,
+              onComplete: () => {
+                cancelAnimationFrame(battleAnimationId)
+                animate()
+                document.querySelector('#userInterface').style.display = 'none'
+
+                gsap.to('#overlappingDiv', {
+                  opacity: 0
+                })
+
+                battle.initiated = false
+                audio.Map.play()
+              }
+            })
+          })
+        }
+      })
+    })
+
+    button.addEventListener('mouseenter', (e) => {
+      const selectedAttack = attacks[e.currentTarget.innerHTML]
+      document.querySelector('#attackType').innerHTML = selectedAttack.type
+      document.querySelector('#attackType').style.color = selectedAttack.color
+    })
+  })
+}
+
+function animateBattle() {
+  battleAnimationId = window.requestAnimationFrame(animateBattle)
+  battleBackground.draw()
+
+  console.log(battleAnimationId)
+
+  renderedSprites.forEach((sprite) => {
+    sprite.draw()
+  })
+}
+
+animate()
+// BattleScene()
+// animateBattle()
+
+document.querySelector('#dialogueBox').addEventListener('click', (e) => {
+  if (queue.length > 0) {
+    queue[0]()
+    queue.shift()
+  } else e.currentTarget.style.display = 'none'
+})
---
+++ Fass World/data/attacks.js
@@ -0,0 +1,26 @@
+const attacks = {
+  Tackle: {
+    name: 'Tackle',
+    damage: 10,
+    type: 'Normal',
+    color: 'black'
+  },
+  Fireball: {
+    name: 'Fireball',
+    damage: 25,
+    type: 'Fire',
+    color: 'red'
+  },
+  StarAttack: {
+    name: 'StarAttack',
+    damage: 20,
+    type: 'Light',
+    color: 'yellow'
+  },
+  Iceball: {
+    name: 'Iceball',
+    damage: 25,
+    type: 'Ice',
+    color: 'blue'
+  }
+}
